15c94f8462466a7b006f55236133de4f09a6d084
Rifatulislam45 <99613131+rifatulislamr@users.noreply.github.com>
2022-09-29 06:25:54 +0600

    grid and card added

--- src/Components/Riad/Riad.js
+++ src/Components/Riad/Riad.js
@@ -1,38 +1,115 @@
 import React from 'react'
-import { Button, Stack } from 'react-bootstrap'
-import Alert from 'react-bootstrap/Alert';
-
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import './Riad.css';
 function Riad() {
   return (
     <>
-    {[
-      'primary',
-      'secondary',
-      'success',
-      'danger',
-      'warning',
-      'info',
-      'light',
-      'dark',
-    ].map((variant) => (
-      <Alert key={variant} variant={variant}>
-        This is a {variant} alert—check it out!
-      </Alert>
-    ))}
-    <Stack direction="horizontal" gap={2}>
-  <Button as="a" variant="primary">
-     Button as link
-  </Button>
-  <Button as="a" variant="success">
-    Button as link
-   </Button>
- </Stack>
-  </>
+     <Container>
+      <Row >
+      {Array.from({ length: 1}).map((_, idx) => (
+        <Col xs={12} md={8} >
+          <Card style={{ width: '20rem' }}>
+          <a className='link' href='https://www.google.com/' target="_blank" text-decoration= "none" ><Card.Img variant="top" src="https://media.istockphoto.com/photos/in-the-hands-of-trees-growing-seedlings-bokeh-green-background-female-picture-id1181366400?s=612x612" />
+            <Card.Body >
+              <Card.Title style={{color:"black",textDecoration:"none"}}>Card title</Card.Title>
+              <Card.Text style={{color:"black"}}>
+                This is a longer card with supporting text below as a natural
+                
+              </Card.Text>
+            </Card.Body></a>
+          </Card>
+        </Col>
+      ))}
+      
+        <Col xs={6} md={4}>
+        <Card style={{ width: '18rem' }}>
+           <a href='https://www.google.com/' target="_blank"> <Card.Img variant="top" src="https://media.istockphoto.com/photos/in-the-hands-of-trees-growing-seedlings-bokeh-green-background-female-picture-id1181366400?s=612x612" /></a>
+            <Card.Body>
+              <Card.Title style={{color:"black"}}>Card title</Card.Title>
+              <Card.Text style={{color:"black"}}>
+                This is a longer card with supporting text below as a natural
+                
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        
+      </Row>
+      </Container>
+    
+    </>
+    
+    
 
 
-//     <div>
-//         
-//     </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     <CardGroup>
+//     <Card>
+//       <Card.Img variant="top" src="https://media.istockphoto.com/photos/in-the-hands-of-trees-growing-seedlings-bokeh-green-background-female-picture-id1181366400?s=612x612" />
+//       <Card.Body>
+//         <Card.Title style={{color:"black"}}>Card title</Card.Title>
+//         <Card.Text style={{color:"black"}}>
+//           This is a wider card with supporting text below as a natural lead-in
+//           to additional content. This content is a little bit longer.
+//         </Card.Text>
+//       </Card.Body>
+//       <Card.Footer>
+//         <small className="text-muted">Last updated 3 mins ago</small>
+//       </Card.Footer>
+//     </Card>
+//     <Card>
+//       <Card.Img variant="top" src="https://media.istockphoto.com/photos/in-the-hands-of-trees-growing-seedlings-bokeh-green-background-female-picture-id1181366400?s=612x612" />
+//       <Card.Body>
+//         <Card.Title style={{color:"black"}}>Card title</Card.Title>
+//         <Card.Text style={{color:"black"}}>
+//           This card has supporting text below as a natural lead-in to
+//           additional content.{' '}
+//         </Card.Text>
+//       </Card.Body>
+//       <Card.Footer>
+//         <small className="text-muted">Last updated 3 mins ago</small>
+//       </Card.Footer>
+//     </Card>
+//     <Card>
+//       <Card.Img variant="top" src="https://media.istockphoto.com/photos/in-the-hands-of-trees-growing-seedlings-bokeh-green-background-female-picture-id1181366400?s=612x612" />
+//       <Card.Body>
+//         <Card.Title style={{color:"black"}}>Card title</Card.Title>
+//         <Card.Text style={{color:"black"}}>
+//           This is a wider card with supporting text below as a natural lead-in
+//           to additional content. This card has even longer content than the
+//           first to show that equal height action.
+//         </Card.Text>
+//       </Card.Body>
+//       <Card.Footer>
+//         <small className="text-muted">Last updated 3 mins ago</small>
+//       </Card.Footer>
+//     </Card>
+//   </CardGroup>
   )
 }
 
