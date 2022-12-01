875219bf0131308ba121b5d1b5db1105eef82f4c
Rifatulislam45 <99613131+rifatulislamr@users.noreply.github.com>
2022-12-01 09:04:17 +0600

    add messenger in my app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Riad from './Components/Riad/Riad';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       <h1>Rifatul Islam Riad</h1>
-       <Riad></Riad>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+         
+          <h2>Welcome to Agro Dairy Firm</h2>
+          <h3>Provider RIFATUL ISLAM RIAD</h3>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
 }
 
 export default App;
