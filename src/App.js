261aeb88156a00af5a26aff259fb599ee111262e
Rifatulislam45 <99613131+rifatulislamr@users.noreply.github.com>
2022-12-02 19:23:56 +0600

    added some code and updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { MessengerChat } from "react-messenger-chat-plugin";
+
 
 class App extends Component {
   render() {
@@ -12,6 +13,35 @@ class App extends Component {
           <h3>Provider RIFATUL ISLAM RIAD</h3>
           <h4>change </h4>
         </div>
+        <MessengerChat
+    pageId="455120307946675"
+    language="English"
+    themeColor={"#000000"}
+    bottomSpacing={20}
+    loggedInGreeting="loggedInGreeting"
+    loggedOutGreeting="loggedOutGreeting"
+    greetingDialogDisplay={"show"}
+    debugMode={true}
+    onMessengerShow={() => {
+      console.log("onMessengerShow");
+    }}
+    onMessengerHide={() => {
+      console.log("onMessengerHide");
+    }}
+    onMessengerDialogShow={() => {
+      console.log("onMessengerDialogShow");
+    }}
+    onMessengerDialogHide={() => {
+      console.log("onMessengerDialogHide");
+    }}
+    onMessengerMounted={() => {
+      console.log("onMessengerMounted");
+    }}
+    onMessengerLoad={() => {
+      console.log("onMessengerLoad");
+    }}
+  />
+
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
