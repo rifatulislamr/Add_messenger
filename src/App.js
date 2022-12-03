0f5ebe3a5b2b93b04afa21c06567de4523c77cb2
Rifatulislam45 <99613131+rifatulislamr@users.noreply.github.com>
2022-12-03 18:23:58 +0600

    added all code finally

--- src/App.js
+++ src/App.js
@@ -1,54 +1,71 @@
-import React, { Component } from 'react';
-import './App.css';
-import { MessengerChat } from "react-messenger-chat-plugin";
+import React from 'react'
+import axios from 'axios'
+import { useState } from 'react';
 
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-         
-          <h2>Welcome to Agro Dairy Firm</h2>
-          <h3>Provider RIFATUL ISLAM RIAD</h3>
-          <h4>change </h4>
+function App() {
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=86b4286db31ce323f5a0c32467615914`
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
+    }
+  }
+
+  return (
+    <div className="app">
+      <div className="search">
+        <input
+          value={location}
+          onChange={event => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder='Enter Location'
+          type="text" />
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temp">
+            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
+          </div>
+          <div className="description">
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
         </div>
-        <MessengerChat
-    pageId="455120307946675"
-    language="English"
-    themeColor={"#000000"}
-    bottomSpacing={20}
-    loggedInGreeting="loggedInGreeting"
-    loggedOutGreeting="loggedOutGreeting"
-    greetingDialogDisplay={"show"}
-    debugMode={true}
-    onMessengerShow={() => {
-      console.log("onMessengerShow");
-    }}
-    onMessengerHide={() => {
-      console.log("onMessengerHide");
-    }}
-    onMessengerDialogShow={() => {
-      console.log("onMessengerDialogShow");
-    }}
-    onMessengerDialogHide={() => {
-      console.log("onMessengerDialogHide");
-    }}
-    onMessengerMounted={() => {
-      console.log("onMessengerMounted");
-    }}
-    onMessengerLoad={() => {
-      console.log("onMessengerLoad");
-    }}
-  />
 
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {data.name !== undefined &&
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+              <p>Wind Speed</p>
+            </div>
+          </div>
+        }
+
+
 
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+
 
 export default App;
